f0e33b4409da3d7327ca5b335c40704724ca8a53
JesusLuvsYooh <57072365+JesusLuvsYooh@users.noreply.github.com>
2021-09-03 13:52:58 +0100

    Update BuildVersionProcessor.cs
    Version 1.1:
    // Fixed where matching numbers like 6.0.6 would go to 7.0.7 (should have been 6.0.7)

--- BuildVersionProcessor.cs
+++ BuildVersionProcessor.cs
@@ -1,11 +1,15 @@
-// Version 1.0  Credits:
+// Credits:
 // JamesDev for original script inspiration
 // && JesusLuvsYooh StephenAllenGames.co.uk for auto build feature and editing.
+// && Nick for beating me to a fix
 
-// This file must be in an "Editor" folder
+// This file must be in an "Editor" folder (Unity/Assets/Editor) for example. 
 // Set autoIncreamentBuildVersion to false, to disable auto version changing
 // This script should increase the end number of your version
 // So..  0 would become 1    -     1.1 would become 1.2    -    1.2.3 would become 1.2.4
+
+// Version 1.1:
+// Fixed where matching numbers like 6.0.6 would go to 7.0.7 (should have been 6.0.7)
 
 using UnityEditor;
 using UnityEditor.Build;
@@ -39,8 +43,8 @@ class BuildVersionProcessor : IPreprocessBuildWithReport
         {
             int versionIncremented = int.Parse(versionParts[versionParts.Length - 1]);
             versionIncremented += 1;
-            string versionFinal = versionCurrent.Replace(versionParts[versionParts.Length - 1], versionIncremented + "");
-            PlayerSettings.bundleVersion = versionFinal;
+            versionParts[versionParts.Length - 1] = versionIncremented.ToString();
+            PlayerSettings.bundleVersion = string.Join('.', versionParts);
 
             Debug.Log("Version:  " + versionCurrent + "  increased to:  " + PlayerSettings.bundleVersion);
         }
